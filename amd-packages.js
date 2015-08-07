6d5274660f4ebad5a345dcd324344d076a118095
phusick <phusick@gmail.com>
2015-08-07 10:18:52 +0200

    Initial commit.

---
+++ amd-packages.js
@@ -0,0 +1,11 @@
+require({packages: [
+    { name: "dgrid", location: "https://cdn.rawgit.com/SitePen/dgrid/v0.3.1"},
+    { name: "put-selector", location: "https://cdn.rawgit.com/kriszyp/put-selector/v0.3.0"},
+    { name: "xstyle", location: "https://cdn.rawgit.com/kriszyp/xstyle/v0.0.5"},
+    { name: "doh", location: "https://cdn.rawgit.com/dojo/util/1.8.0/doh"},
+    { name: "rql", location: "https://cdn.rawgit.com/persvr/rql/v0.3.1"},
+    { name: "data", location: "https://cdn.rawgit.com/phusick/cdn/jsfiddle/data"}
+    ]
+});
+
+// https://cdn.rawgit.com/dojo/dijit/1.8.0/themes/claro/claro.css
